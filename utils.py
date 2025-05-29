dae048a2064fa3a4e45a492a164b3b654101167f
guirochaluz <157293262+guirochaluz@users.noreply.github.com>
2025-05-29 13:30:44 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,25 @@
 from sqlalchemy import create_engine
-from datetime import datetime
-import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+import requests
 
+# Carrega .env
 load_dotenv()
+
+# Configura banco de dados
 DB_URL = os.getenv("DB_URL")
-engine = create_engine(DB_URL)
+engine = create_engine(
+    DB_URL,
+    pool_size=5,
+    max_overflow=10,
+    pool_timeout=30
+)
+
+# Data de corte padrão para filtros ou fees
 DATA_INICIO = datetime(2024, 5, 16)
 
+# Função para buscar a taxa ML
 def buscar_ml_fee(order_id: str, access_token: str):
     url = f"https://api.mercadolibre.com/orders/{order_id}?access_token={access_token}"
     try:
