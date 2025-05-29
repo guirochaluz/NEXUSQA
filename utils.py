a82d940cb4cfb33e6b14ab36a09b183b6883873e
guirochaluz <157293262+guirochaluz@users.noreply.github.com>
2025-05-29 13:28:47 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,25 @@
+from sqlalchemy import create_engine
+from datetime import datetime
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DB_URL = os.getenv("DB_URL")
+engine = create_engine(DB_URL)
+DATA_INICIO = datetime(2024, 5, 16)
+
+def buscar_ml_fee(order_id: str, access_token: str):
+    url = f"https://api.mercadolibre.com/orders/{order_id}?access_token={access_token}"
+    try:
+        resp = requests.get(url, timeout=10)
+        if resp.ok:
+            full_order = resp.json()
+            payments = full_order.get("payments", [])
+            if payments:
+                fee = payments[0].get("marketplace_fee")
+                return (order_id, fee)
+    except Exception as e:
+        print(f"[erro buscar_ml_fee] {order_id}: {e}")
+    return (order_id, None)
 
