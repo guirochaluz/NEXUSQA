09aa3c0bbd12f4c66296f1fff5f57c74220901bd
guirochaluz <157293262+guirochaluz@users.noreply.github.com>
2025-05-30 09:50:02 -0300

    Delete #!/bin directory

--- #!/bin/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Inicia FastAPI (backend) na porta 8000
-uvicorn api:app --host 0.0.0.0 --port 8000 &
-
-# Inicia Streamlit (frontend) na porta 8501
-streamlit run app.py --server.port 8501 --server.address=0.0.0.0 --server.enableXsrfProtection false
