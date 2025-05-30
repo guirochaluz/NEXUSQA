1e02464464c82f23619f95c514987a0ca17af726
guirochaluz <157293262+guirochaluz@users.noreply.github.com>
2025-05-30 09:46:52 -0300

    Update start.sh

--- #!/bin/start.sh
+++ #!/bin/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Inicia o backend (FastAPI) na porta 8000
+# Inicia FastAPI (backend) na porta 8000
 uvicorn api:app --host 0.0.0.0 --port 8000 &
 
-# Inicia o frontend (Streamlit) na porta 8501
-streamlit run app.py --server.port 8501 --server.address=0.0.0.0
+# Inicia Streamlit (frontend) na porta 8501
+streamlit run app.py --server.port 8501 --server.address=0.0.0.0 --server.enableXsrfProtection false
